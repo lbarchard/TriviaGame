adbfe770c861f1c3f5d760b34f774aced7968086
Barchard <lb185070@ncr.com>
2017-02-25 16:48:21 -0500

    building initial game load and reload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,31 @@
 $(document).ready(function() {
-var = possibleQuestions [{
-    question: "question 1",
-    correctAnswer: "correct answer to question 1",
-    trickAnswers: ["first trick answer to question 1"],
-    image: "image.png"
-}]
+// var possibleQuestions [{
+//     question: "question 1",
+//     correctAnswer: "correct answer to question 1",
+//     trickAnswers: ["first trick answer to question 1"],
+//     image: "image.png"
+// }]
 
-var timeToAnswerQuestions = 30;
-var timeToWaitBeforeNextQuestion = 10;
-var correctAnswerCount = 0;
-var incorrectAnswerCount = 0;
-var unansweredQuestionsCount = 0;
+var gameTracking = {}
+
+function resetGameTracking() {
+    gameTracking.timeToAnswerQuestions = 30;
+    gameTracking.timeToAnswerQuestions = 30;
+    gameTracking.timeToWaitBeforeNextQuestion = 10;
+    gameTracking.correctAnswerCount = 0;
+    gameTracking.incorrectAnswerCount = 0;
+    gameTracking.unansweredQuestionsCount = 0;
+};
 
 
-}
+function startGame(){
+    // This function begins the game whether starting for the first time or restarting
+    resetGameTracking();
+
+};
+
+$("#Start").on("click", startGame);
+$("#Restart").on("click", startGame);
+
+
+});

31d102eb3f1fbf226523f37b1d3746694ba07d4f
Barchard <lb185070@ncr.com>
2017-02-25 16:16:12 -0500

    staging initial variables needed for the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
+var = possibleQuestions [{
+    question: "question 1",
+    correctAnswer: "correct answer to question 1",
+    trickAnswers: ["first trick answer to question 1"],
+    image: "image.png"
+}]
+
+var timeToAnswerQuestions = 30;
+var timeToWaitBeforeNextQuestion = 10;
+var correctAnswerCount = 0;
+var incorrectAnswerCount = 0;
+var unansweredQuestionsCount = 0;
+
 
 }

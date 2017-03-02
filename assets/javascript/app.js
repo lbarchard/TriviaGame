771c48af4dec4efb461ed45da004c78b60f24c5f
Barchard <lb185070@ncr.com>
2017-03-01 21:17:45 -0500

    got cycle back to start working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,8 +45,6 @@ function displayTimer(secondsGoneBy) {
     $("#timeRemaining").html(secondsGoneBy);
 }
 
-
-
 function checkIfTimeOut() {
     var currentTime = (new Date()).getTime();
     var secondsGoneBy = Math.floor((currentTime - beginningTimeOfTimer)/1000);
@@ -55,7 +53,7 @@ function checkIfTimeOut() {
         setTimeout(checkIfTimeOut, 1000);
     }
     else {
-        displayTimeOutScreen();        
+        displayResultsScreen();        
     }
 }
 
@@ -85,101 +83,100 @@ function setTimer() {
 
 
 //Active area of development
-function displayTimeOutScreen() {
+function displayResultsScreen() {
     console.log("You timed out")
 }
 
-function displayRightAnswerScreen() {
-    $("#result").html("You got it right");
+function displayResultsScreen() {
+    $("result").html(gameTracking.result);
     $("#correctAnswer").html("The correct answer was: " + possibleQuestions[gameTracking.currentQuestion].correctAnswer);
+    $("correctAnswerCount").html("Correct Answers:" + possibleQuestions[gameTracking.correctAnswerCount]);
+    $("incorrectAnswerCount").html("Incorrect Answers:" + possibleQuestions[gameTracking.incorrectAnswerCount]);
+    $("unansweredQuestionsCount").html("Timeout Questions:" + possibleQuestions[gameTracking.unansweredQuestionsCount]);
+    $(".resultsScreen").show();    
+    $(".notResultsScreen").hide();
+
 }
 
-function displayWrongAnswerScreen() {
-    $("#result").html("You got it wrong");
-    $("#correctAnswer").html("The correct answer was: " + possibleQuestions[gameTracking.currentQuestion].correctAnswer);
-}
 
 
-function checkIfRightAnswer(button) {
-    if ($(button).html()===possibleQuestions[gameTracking.currentQuestion-1].correctAnswer) {
-        displayRightAnswerScreen();
+
+function checkResult(button) {
+    if ($(button).html()===possibleQuestions[gameTracking.currentQuestion].correctAnswer) {
+        gameTracking.correctAnswerCount = gameTracking.correctAnswerCount + 1;
+        gameTracking.result = "You got it right!";
+    }
+    else if ($(button).html()!=null) {
+        gameTracking.incorrectAnswerCount = gameTracking.incorrectAnswerCount + 1;
+        gameTracking.result = "You got it wrong!";
     }
     else {
-        displayWrongAnswerScreen();
-    }
-}
-
-//Active area of development
-
-function displayQuestionScreen() {
-    $(".questionScreen").show();
-    $(".notQuestionScreen").hide();
+        gameTracking.result = "You timed out!";
+    };
+    setTimeout(displayQuestionScreen, gameTracking.timeToWaitBeforeNextQuestion);
+    displayResultsScreen();
 };
-//clean this up
-function getNextQuestion() {
-        
-        if (gameTracking.currentQuestion<possibleQuestions.length) {
-                questionTimeout = setTimeout(displayTimeOutScreen, gameTracking.timeToAnswerQuestions);
-                $("#answerOne").html(possibleQuestions[gameTracking.currentQuestion].correctAnswer);
-                $("#answerTwo").html(possibleQuestions[gameTracking.currentQuestion].trickAnswer1);
-                $("#answerThree").html(possibleQuestions[gameTracking.currentQuestion].trickAnswer2);
-                $("#answerFour").html(possibleQuestions[gameTracking.currentQuestion].trickAnswer3)
-                $("#question").html(possibleQuestions[gameTracking.currentQuestion].question);
-                gameTracking.currentQuestion = gameTracking.currentQuestion + 1
+
+function getNextQuestion() {        
+        if (gameTracking.currentQuestion<possibleQuestions.length-1) {
+                                gameTracking.currentQuestion = gameTracking.currentQuestion + 1
         }
         else {
-            newGame();
+            displayStartScreen();
         }
 };
 
-function nextQuestion(){
-    getNextQuestion();
-    displayQuestionScreen();
+function displayQuestionScreen() {
+    getNextQuestion()
+    $("#answerOne").html(possibleQuestions[gameTracking.currentQuestion].correctAnswer);
+    $("#answerTwo").html(possibleQuestions[gameTracking.currentQuestion].trickAnswer1);
+    $("#answerThree").html(possibleQuestions[gameTracking.currentQuestion].trickAnswer2);
+    $("#answerFour").html(possibleQuestions[gameTracking.currentQuestion].trickAnswer3)
+    $("#question").html(possibleQuestions[gameTracking.currentQuestion].question);
+    $(".questionScreen").show();
+    $(".notQuestionScreen").hide();
+    
+};
 
+function initializeNewGame() {
+    gameTracking.timeToAnswerQuestions = 3000;
+    gameTracking.timeToWaitBeforeNextQuestion = 3000;
+    gameTracking.correctAnswerCount = 0;
+    gameTracking.incorrectAnswerCount = 0;
+    gameTracking.unansweredQuestionsCount = 0;
+    gameTracking.currentQuestion = -1;
+    gameTracking.result = "";
 };
 
 function displayStartScreen() {
+    initializeNewGame();
     $(".startScreen").show();    
     $(".notStartScreen").hide();
 }
 
-function initializeNewGame() {
-    gameTracking.timeToAnswerQuestions = 10000;
-    gameTracking.timeToWaitBeforeNextQuestion = 10000;
-    gameTracking.correctAnswerCount = 0;
-    gameTracking.incorrectAnswerCount = 0;
-    gameTracking.unansweredQuestionsCount = 0;
-    gameTracking.currentQuestion = 0;
-};
+displayStartScreen();
 
-function newGame() {
-    initializeNewGame();
-    displayStartScreen();
-};
-
-newGame();
-
-
-$("#startButton").on("click", nextQuestion);
+//**************Listening for Button Clicks*********************/
+$("#startButton").on("click", displayQuestionScreen);
 
 $("#answerOne").on("click", (function() {
     clearInterval(questionTimeout);
-    checkIfRightAnswer("#answerOne");
+    checkResult("#answerOne");
 }));
 
 $("#answerTwo").on("click", (function() {
     clearInterval(questionTimeout);
-    checkIfRightAnswer("#answerTwo");
+    checkResult("#answerTwo");
 }));
 
 $("#answerThree").on("click", (function() {
     clearInterval(questionTimeout);
-    checkIfRightAnswer("#answerThree");
+    checkResult("#answerThree");
 }));
 
 $("#answerFour").on("click", (function() {
     clearInterval(questionTimeout);
-    checkIfRightAnswer("#answerFour");
+    checkResult("#answerFour");
 }));
 
 });
